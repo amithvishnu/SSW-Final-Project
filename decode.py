e09c1683b0f4e34ccf9ed21b22a1d1813af92c82
unknown <amithvishnusuresh@gmail.com>
2022-12-14 16:26:07 -0500

    Initial commit

---
+++ decode.py
@@ -0,0 +1,32 @@
+import json
+import time
+import csv
+from MRTD import HardwareScanner, decode, mismatch
+
+records_encoded_file = open('records_encoded.json','r')
+
+records_encoded_file = json.load(records_encoded_file)
+
+f = open('decode_execution_time.csv', 'w')
+writer = csv.writer(f)
+writer.writerow(["K", "Without test", "With test"])
+
+counter = 0
+step_thousand = 0
+start_time = time.perf_counter()
+for i in records_encoded_file['records_encoded']:
+	HardwareScanner.MRZ = i
+	counter+=1
+	print(decode())
+	if(counter==100):
+		end_time = time.perf_counter()
+		# print(f"Total time consumed to decode {counter} records is {end_time-start_time:0.8f} seconds")
+		writer.writerow([str(counter), str(end_time-start_time), ""])
+
+	if(counter==1000*(step_thousand+1)):
+		end_time = time.perf_counter()
+		step_thousand+=1
+		# print(f"Total time consumed to decode {counter} records is {end_time-start_time:0.8f} seconds")
+		writer.writerow([str(counter), str(end_time-start_time), ""])
+
+	
